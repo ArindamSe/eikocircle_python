402841fdf7ef2ca1c8b275ec9e8f6472269c4946
KRITGYA2001 <kritgyakumar92@gmail.com>
2023-04-25 14:09:58 +0530

    changes

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from Common.models import Common    
+
+class Brand(Common):
+    brand_name = models.CharField()
+    
+class User(AbstractUser, Common):
+    ROLES = (
+        ('brand', 'Brand'),
+        ('refurbisher', 'Refurbisher'),
+        ('collection_center', 'Collection Center'),
+        ('recycling_unit', 'Recycling Unit'),
+        ('individual_consumer', 'Individual Consumer'),
+        ('bulk_consumer', 'Bulk Consumer')
+    )
+
+    role = models.CharField(max_length=20, choices=ROLES, default='brand')
+    company_name = models.CharField(max_length=50, blank=True, null=True)
+    address = models.CharField(max_length=250, blank=True, null=True)
+    mobile = models.CharField(max_length=10, blank=True, null=True)
+    
+
+    class Meta:
+        db_table = "Users"
+        verbose_name = "User"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
