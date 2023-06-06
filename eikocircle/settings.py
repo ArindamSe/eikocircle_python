b95b578f123403384775ad46cdaef7b9eac6a1e5
KRITGYA2001 <kritgyakumar92@gmail.com>
2023-06-06 17:16:29 +0530

    changes

--- eikocircle/settings.py
+++ eikocircle/settings.py
@@ -163,8 +163,8 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=45),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
     'ROTATE_REFRESH_TOKEN': False,
     'BLACKLIST_AFTER_ROTATION': False
 }
